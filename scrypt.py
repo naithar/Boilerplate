a8e744ab8d09b1ec52dfb4a10452ee6939eae756
Sergey Minakov <s.minakov@be-interactive.ru>
2017-01-23 11:56:35 +0300

    initial implementation

--- scrypt.py
+++ scrypt.py
@@ -1,1 +1,35 @@
-print 'boilerplate'
+#!/usr/bin/python
+
+
+
+def help():
+    print 'python boilderplace scrypt'
+
+def run(filename, toFilename, parameters):
+    string = ''
+    with open(filename, "r") as original:
+        string = original.read()
+        original.close()
+
+    print 'Original file content:'
+    print string
+
+    print 'Parameters:'
+    print parameters
+
+    newString = string.format(**parameters)
+    print 'Formated file content:'
+    print newString
+
+    with open(toFilename, "w") as new:
+        new.write(newString)
+        new.close()
+
+def main(args):
+    print __name__
+    print args
+
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:])
---
+++ test.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+func foo(a: Int?) {
+}
+
+foo(0 as? Int)
